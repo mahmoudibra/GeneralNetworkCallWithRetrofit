96bcbaa13d42287c48c6e0626ae74682ecc8972d
Mahmoud ibrahim <m.ibrahim@saned.sa>
2018-09-12 16:29:58 +0200

    Update package name

--- app/src/main/java/com/sanedapps/generalnetworkcall/Network/NetworkEndpoint/OrderEndPoint.java
+++
@@ -1,9 +0,0 @@
-package com.sanedapps.generalnetworkcall.Network.NetworkEndpoint;
-
-/**
- * Created by Mahmoud on 2/14/18.
- */
-public interface OrderEndPoint {
-
-
-}
--- app/src/main/java/com/sanedapps/generalnetworkcall/Network/Repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sanedapps.generalnetworkcall.Network.Repository;
-
-/**
- * Created by Mahmoud on 2/14/18.
- */
-public class OrderRepository {
-
-   
-}
